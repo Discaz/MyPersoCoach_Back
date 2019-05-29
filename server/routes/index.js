8b79cbbfa20fdc53e9fe8d6a4d7ec38c0c621c4b
Kamel Belkheiri <kamel.belkheiri75@gmail.com>
2019-05-29 17:40:12 +0200

    Revert "server architecture"
    This reverts commit 26cf69b6ccfc5efc02a334fbf5b81258e6e92277.

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-const auth = require("./auth")
-const login = require("./login")
-const main_theme = require ("./main_theme")
-const problem = require ("./problem")
-const question = require ("./question")
-const problem_origin = require ("./problem_origin")
-const text_static = require ("./text_static")
-
-module.exports = {auth, login, main_theme, problem, question, problem_origin, text_static}
