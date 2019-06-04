3ff3529c73af914d1322e47ba7224426b2d12df6
Discaz <olivier.discazeaux@gmail.com>
2019-06-04 17:38:51 +0200

    roads in icon, question, problem_origin

--- server/routes/problem_origin.js
+++ server/routes/problem_origin.js
@@ -2,102 +2,98 @@ const express = require ('express');
 const router = express.Router();
 const connection = require('../config')
 
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({
+    extended: true })
+);
 
 
 router.get ('/', (req, res) =>{
-  res.status(200).send('problem_origin page ! ')
-});
-// router.get ('/', (req, res) =>{
-//     connection.query('SELECT * FROM  problem_origin',(err, results) => {
+    connection.query('SELECT * FROM  problem_origin',(err, results) => {
 
-//         if (err) {
+        if (err) {
    
-//           // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
-//           res.status(500).send('Erreur lors de la récupération de problem_origin');
-//         } else {
+          
+          res.status(500).send('Erreur lors de la récupération de problem_origin');
+        } else {
    
-//           // Si tout s'est bien passé, on envoie le résultat de la requête SQL en tant que JSON.
-//           res.json(results);
-//         }
-//       })
-//     });
+          
+          res.json(results);
+        }
+      })
+    });
 
 
-
-
-
-//     router.get ('/:id', (req, res) =>{
-//         connection.query('SELECT * FROM problem_origin',(err, results) => {
-            
-//             if (err) {
-       
-//               // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
-//               res.status(500).send('Erreur lors de la récupération de problem_origin');
-//             } else {
-       
-//               // Si tout s'est bien passé, on envoie le résultat de la requête SQL en tant que JSON.
-//               res.json(results);
-//             }
-//           })
-//         });
-    
-
-//         router.post ('/api/problem_origin', (req, res) =>{
-//             const formData = req.body;
-//             connection.query('INSERT INTO problem_origin ...SET ?', formData, (err, results) => {
+router.get ('/:id', (req, res) =>{
+    connection.query('SELECT * FROM problem_origin',(err, results) => {
         
-//                 if (err) {
-           
-//                   // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
-//                   res.status(500).send('Erreur lors de la récupération de problem_origin');
-//                 } else {
-           
-//                   // Si tout s'est bien passé, on envoie le résultat de la requête SQL en tant que JSON.
-//                   res.json(results);
-//                 }
-//               })
-//             });
+        if (err) {
     
+          
+          res.status(500).send('Erreur lors de la récupération de problem_origin');
+        } else {
+    
+          
+          res.json(results);
+        }
+      })
+    });
+    
+
+router.post ('/', (req, res) =>{
+const formData = req.body;
+connection.query('INSERT INTO problem_origin ...SET ?', formData, (err, results) => {
+
+    if (err) {
+
+      
+      res.status(500).send('Erreur lors de la récupération de problem_origin');
+    } else {
+
+      res.json(results);
+    }
+  })
+});
+
             
-//             router.put('/api/problem_origin/:id', (req, res) => {
-
-//                 // récupération des données envoyées
-//                 const idproblem_origin = req.params.id;
-//                 const formData = req.body;
-               
-//                 // connection à la base de données, et insertion du film
-//                 connection.query('UPDATE problem SET ? WHERE id = ?', [formData, idproblem_origin], err => {
-               
-//                   if (err) {
-//                     // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
-//                     console.log(err);
-//                     res.status(500).send("Erreur lors de la mise a jour de problem_origin");
-//                   } else {
-               
-//                     // Si tout s'est bien passé, on envoie un statut "ok".
-//                     res.sendStatus(200);
-//                   }
-//                 });
-//                });
+router.put('/:id', (req, res) => {
 
 
+const idproblem_origin = req.params.id;
+const formData = req.body;
 
-//                router.delete('/api/problem_origin/:id', (req, res) => {
-//                 const idproblem_origin = req.params.id;
-//                 // TODO supprimer les données (étape 2)
-//                 connection.query('DELETE FROM problem_origin WHERE id = ?', [idproblem_origin], err => {
-//                     // TODO envoyer une réponse au client (étape 3)
-//                     if (err) {
-//                         console.log(err);
-//                         res.status(500).send("Erreur lors de la suppression d'un id de problem_origin");
-//                       } else {
-//                         res.sendStatus(200);
-//                       }
-//                 });;
-//               });
+
+connection.query('UPDATE problem SET ? WHERE id = ?', [formData, idproblem_origin], err => {
+
+  if (err) {
+  
+    console.log(err);
+    res.status(500).send("Erreur lors de la mise a jour de problem_origin");
+  } else {
+
+   
+    res.sendStatus(200);
+  }
+});
+});
+
+
+
+router.delete('/:id', (req, res) => {
+const idproblem_origin = req.params.id;
+
+connection.query('DELETE FROM problem_origin WHERE id = ?', [idproblem_origin], err => {
+    
+    if (err) {
+        console.log(err);
+        res.status(500).send("Erreur lors de la suppression d'un id de problem_origin");
+      } else {
+        res.sendStatus(200);
+      }
+});;
+});
              
-
-
-
 
 module.exports = router
--- server/routes/question.js
+++ server/routes/question.js
@@ -2,103 +2,98 @@ const express = require ('express');
 const router = express.Router();
 const connection = require('../config')
 
+const bodyParser = require('body-parser');
 
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({
+    extended: true })
+);
 
 
 router.get ('/', (req, res) =>{
-  res.status(200).send('Question page ! ')
-});
-// router.get ('/', (req, res) =>{
-//     connection.query('SELECT * FROM  question',(err, results) => {
+    connection.query('SELECT * FROM question',(err, results) => {
 
-//         if (err) {
+        if (err) {
    
-//           // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
-//           res.status(500).send('Erreur lors de la récupération de question');
-//         } else {
+          
+          res.status(500).send('Erreur lors de la récupération de question');
+        } else {
    
-//           // Si tout s'est bien passé, on envoie le résultat de la requête SQL en tant que JSON.
-//           res.json(results);
-//         }
-//       })
-//     });
+          
+          res.json(results);
+        }
+      })
+    });
 
-
-
-
-
-//     router.get ('/:id', (req, res) =>{
-//         connection.query('SELECT * FROM question',(err, results) => {
-            
-//             if (err) {
-       
-//               // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
-//               res.status(500).send('Erreur lors de la récupération du question');
-//             } else {
-       
-//               // Si tout s'est bien passé, on envoie le résultat de la requête SQL en tant que JSON.
-//               res.json(results);
-//             }
-//           })
-//         });
-    
-
-//         router.post ('/api/question', (req, res) =>{
-//             const formData = req.body;
-//             connection.query('INSERT INTO question ...SET ?', formData, (err, results) => {
+router.get ('/:id', (req, res) =>{
+    connection.query('SELECT * FROM question',(err, results) => {
         
-//                 if (err) {
-           
-//                   // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
-//                   res.status(500).send('Erreur lors de la récupération du question');
-//                 } else {
-           
-//                   // Si tout s'est bien passé, on envoie le résultat de la requête SQL en tant que JSON.
-//                   res.json(results);
-//                 }
-//               })
-//             });
+        if (err) {
     
-            
-//             router.put('/api/question/:id', (req, res) => {
+          
+          res.status(500).send('Erreur lors de la récupération du question');
+        } else {
+    
+          
+          res.json(results);
+        }
+      })
+    });
+    
 
-//                 // récupération des données envoyées
-//                 const idquestion = req.params.id;
-//                 const formData = req.body;
-               
-//                 // connection à la base de données, et insertion du film
-//                 connection.query('UPDATE question SET ? WHERE id = ?', [formData, idquestion], err => {
-               
-//                   if (err) {
-//                     // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
-//                     console.log(err);
-//                     res.status(500).send("Erreur lors de la mise a jour de question");
-//                   } else {
-               
-//                     // Si tout s'est bien passé, on envoie un statut "ok".
-//                     res.sendStatus(200);
-//                   }
-//                 });
-//                });
+router.post ('/', (req, res) =>{
+    const formData = req.body;
+    connection.query('INSERT INTO question ...SET ?', formData, (err, results) => {
+
+        if (err) {
+    
+          
+          res.status(500).send('Erreur lors de la récupération du question');
+        } else {
+    
+          
+          res.json(results);
+        }
+      })
+    });
+    
+    
+router.put('/:id', (req, res) => {
+
+    
+    const idquestion = req.params.id;
+    const formData = req.body;
+    
+    
+    connection.query('UPDATE question SET ? WHERE id = ?', [formData, idquestion], err => {
+    
+      if (err) {
+        
+        console.log(err);
+        res.status(500).send("Erreur lors de la mise a jour de question");
+      } else {
+    
+        
+        res.sendStatus(200);
+      }
+    });
+    });
 
 
 
-//                router.delete('/api/question/:id', (req, res) => {
-//                 const idquestion = req.params.id;
-//                 // TODO supprimer les données (étape 2)
-//                 connection.query('DELETE FROM question WHERE id = ?', [idquestion], err => {
-//                     // TODO envoyer une réponse au client (étape 3)
-//                     if (err) {
-//                         console.log(err);
-//                         res.status(500).send("Erreur lors de la suppression d'un id de question");
-//                       } else {
-//                         res.sendStatus(200);
-//                       }
-//                 });;
-//               });
+router.delete('/:id', (req, res) => {
+const idquestion = req.params.id;
+
+connection.query('DELETE FROM question WHERE id = ?', [idquestion], err => {
+    
+    if (err) {
+        console.log(err);
+        res.status(500).send("Erreur lors de la suppression d'un id de question");
+      } else {
+        res.sendStatus(200);
+      }
+});;
+});
              
-
-
-
 
 module.exports = router
