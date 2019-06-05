f6eff86441a21f1f721a0bb3ed0cc9233d7e589d
Discaz <olivier.discazeaux@gmail.com>
2019-06-05 18:02:47 +0200

    removal of comments

--- server/routes/main_theme.js
+++ server/routes/main_theme.js
@@ -44,11 +44,11 @@ router.post('/', (req, res) => {
 router.get ('/:id', (req, res) =>{
   connection.query('SELECT * FROM  main_theme',(err, results) => {
       if (err) {
-        //Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
+        
         res.status(500).send('Erreur lors de la récupération du main theme');
       } 
       else {
-        // Si tout s'est bien passé, on envoie le résultat de la requête SQL en tant que JSON.
+        
         res.json(results);
       }
 
@@ -58,18 +58,18 @@ router.get ('/:id', (req, res) =>{
 
 
 router.put('/:id', (req, res) => {
-// récupération des données envoyées
+
 const idmaintheme = req.params.id;
 const formData = req.body;
 
-// connection à la base de données, et insertion du film
+
 connection.query('UPDATE main_theme SET ? WHERE id = ?', [formData, idmaintheme], err => {
     if (err) {
-      // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
+      
       console.log(err);
       res.status(500).send("Erreur lors de la mise a jour de main_theme");
     } else {
-      // Si tout s'est bien passé, on envoie un statut "ok".
+      
       res.sendStatus(200);
     }
 
@@ -80,9 +80,9 @@ connection.query('UPDATE main_theme SET ? WHERE id = ?', [formData, idmaintheme]
 
 router.delete('/:id', (req, res) => {
 const idmaintheme = req.params.id;
-// TODO supprimer les données (étape 2)
+
 connection.query('DELETE FROM main_theme WHERE id = ?', [idmaintheme], err => {
-    // TODO envoyer une réponse au client (étape 3)
+    
     if (err) {
       console.log(err);
       res.status(500).send("Erreur lors de la suppression d'un id main_theme");
@@ -94,9 +94,6 @@ connection.query('DELETE FROM main_theme WHERE id = ?', [idmaintheme], err => {
   });
 
 });
-
-
-
 
 
 module.exports = router
--- server/routes/text_static.js
+++ server/routes/text_static.js
@@ -55,20 +55,20 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
 
-  // récupération des données envoyées
+  
   const idtext_static = req.params.id;
   const formData = req.body;
  
-  // connection à la base de données, et insertion du film
+  
   connection.query('UPDATE text_static SET ? WHERE id = ?', [formData, idtext_static], err => {
  
     if (err) {
-      // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
+      
       console.log(err);
       res.status(500).send("Erreur lors de la mise a jour de text_static");
     } else {
  
-      // Si tout s'est bien passé, on envoie un statut "ok".
+      
       res.sendStatus(200);
     }
   });
@@ -76,9 +76,9 @@ router.put('/:id', (req, res) => {
 
  router.delete('/:id', (req, res) => {
   const idtext_static = req.params.id;
-  // TODO supprimer les données (étape 2)
+ 
   connection.query('DELETE FROM text_static WHERE id = ?', [idtext_static], err => {
-      // TODO envoyer une réponse au client (étape 3)
+     
       if (err) {
           console.log(err);
           res.status(500).send("Erreur lors de la suppression d'un id de text_static");
