9647cd294eb97dea471ed18abf5b4f0917809e0d
Carmine David <debartolocarminedavide@gmail.com>
2019-07-18 15:22:07 +0200

    [2-2] Check email- back

--- server/routes/login.js
+++ server/routes/login.js
@@ -5,9 +5,20 @@ const router = express.Router()
 
 router.get('/',(req, res)=> {
     res.status(200).send('je suis a la racine de login')
-
 })
 
-
+// On enregistre un utilisateur
+router.post('/check-email', (req, res) => {
+  
+    connection.query(`SELECT email FROM manager WHERE email="${req.body.email}"` ,(err, results) => {
+        if (err) {  
+            res.status(500).send('Erreur lors de la récupération données de la table manager');
+        }
+        else {
+            res.json(results);
+        }
+    })
+    
+})
 
 module.exports = router;
